eabdfecff5920c917312024456fe5f14c4aac113
Melika Dabiri <jalehdabiri@gmail.com>
2025-01-20 14:00:20 -0800

    CBC

--- blockCiphers.py
+++ blockCiphers.py
@@ -7,18 +7,29 @@ from Crypto.Random import get_random_bytes
 def ECB(plaintext, key):
     cipher = AES.new(key, AES.MODE_ECB)
     ciphertext : bytes = b''
-    for index in range(16, len(plaintext), 16):
-        block = plaintext[index-16:index]
-        print("b", block)
+    for index in range(0, len(plaintext), 16):
+        block = plaintext[index:index+16]
+        # print("b", block)
         ciphertext += cipher.encrypt(block)
     return ciphertext
 
 
 # CBC Melika
 def CBC(plaintext, key, iv):
-    print("temp")
-    aes_key = get_random_bytes(16)
-    cipher = AES.new(aes_key, AES.MODE_CTR)
+    cipher = AES.new(key, AES.MODE_ECB) # still picking ecb mode for manual implementation
+    ciphertext: bytes = b''
+    prev_block = iv
+
+    for index in range(16, len(plaintext), 16):
+        block = plaintext[index-16:index]
+        # XOR the block with the previous ciphertext block 
+        xored_block = bytes([b1 ^ b2 for b1, b2 in zip(block, prev_block)]) 
+        encrypted_block = cipher.encrypt(xored_block)
+        ciphertext += encrypted_block
+        prev_block = encrypted_block
+
+    return ciphertext    
+
 
 def add_padding(plaintext : bytes, blocksize=16):
     byte_length = len(plaintext)
@@ -44,16 +55,18 @@ def task1():
 
         #encrypt the plaintext
         ciphertext1 = ECB(plaintext, key)
-        # ciphertext2 = CBC(plaintext, key, iv)
+        ciphertext2 = CBC(plaintext, key, iv)
 
         # add the header
-        cypherfile1raw = header + ciphertext1
+        cipherfile1raw = header + ciphertext1
+        cipherfile2raw = header + ciphertext2
 
         # save as new files
         with open("mustangECB.bmp", "wb") as cipherfile1:
-            cipherfile1.write(cypherfile1raw)
-        
+            cipherfile1.write(cipherfile1raw)
 
+        with open("mustangCBC.bmp", "wb") as cipherfile2:
+            cipherfile2.write(cipherfile2raw)
 
 task1()
         
